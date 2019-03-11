17c76f852ee3088114e2c642832437aad6391686
Guillaume Vincent <gvincent@redhat.com>
2019-03-11 08:31:26 +0100

    Update site

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,8 +1,8 @@
-import React from 'react'
-import PropTypes from 'prop-types'
+import React from "react";
+import PropTypes from "prop-types";
 
 const Header = props => (
-  <header id="header" style={props.timeout ? { display: 'none' } : {}}>
+  <header id="header" style={props.timeout ? { display: "none" } : {}}>
     <div className="logo">
       <span className="icon fa-worker" />
     </div>
@@ -10,9 +10,9 @@ const Header = props => (
       <div className="inner">
         <h1>Creasanit</h1>
         <p>
-          Creasanit est une entreprise de plomberie installé en Aquitaine. Nous
-          sommes présents sur toute la région afin de vous aider dans la
-          réalisation de vos travaux de salle de bain, sanitaire.
+          Crea Sanit est une entreprise de plomberie installée au pian medoc.
+          nous sommes présents sur toute la région afin de vous aider dans la
+          réalisation de vos travaux de salle de bain, plomberie et chauffage.
         </p>
       </div>
     </div>
@@ -22,20 +22,40 @@ const Header = props => (
           <a
             href="javascript:;"
             onClick={() => {
-              props.onOpenArticle('notre_travail')
+              props.onOpenArticle("notre_activite");
             }}
           >
-            Notre travail
+            Notre activité
           </a>
         </li>
         <li>
           <a
             href="javascript:;"
             onClick={() => {
-              props.onOpenArticle('about')
+              props.onOpenArticle("plomberie_sanitaire");
             }}
           >
-            A propos
+            Plomberie/Sanitaire
+          </a>
+        </li>
+        <li>
+          <a
+            href="javascript:;"
+            onClick={() => {
+              props.onOpenArticle("chauffage_climatisation");
+            }}
+          >
+            Chauffage/Climatisation
+          </a>
+        </li>
+        <li>
+          <a
+            href="javascript:;"
+            onClick={() => {
+              props.onOpenArticle("depannage");
+            }}
+          >
+            Dépannage
           </a>
         </li>
         <li>
@@ -44,11 +64,11 @@ const Header = props => (
       </ul>
     </nav>
   </header>
-)
+);
 
 Header.propTypes = {
   onOpenArticle: PropTypes.func,
-  timeout: PropTypes.bool,
-}
+  timeout: PropTypes.bool
+};
 
-export default Header
+export default Header;
--- src/components/Main.js
+++ src/components/Main.js
@@ -1,8 +1,8 @@
-import React from 'react'
-import PropTypes from 'prop-types'
+import React from "react";
+import PropTypes from "prop-types";
 
-import pic1 from '../images/pic1.jpg'
-import pic2 from '../images/pic2.jpg'
+import pic1 from "../images/pic1.jpg";
+import pic2 from "../images/pic2.jpg";
 
 class Main extends React.Component {
   render() {
@@ -10,70 +10,136 @@ class Main extends React.Component {
       <div
         className="close"
         onClick={() => {
-          this.props.onCloseArticle()
+          this.props.onCloseArticle();
         }}
       />
-    )
+    );
 
     return (
       <div
         ref={this.props.setWrapperRef}
         id="main"
-        style={this.props.timeout ? { display: 'flex' } : { display: 'none' }}
+        style={this.props.timeout ? { display: "flex" } : { display: "none" }}
       >
         <article
-          id="notre_travail"
+          id="notre_activite"
           className={`${
-            this.props.article === 'notre_travail' ? 'active' : ''
-          } ${this.props.articleTimeout ? 'timeout' : ''}`}
-          style={{ display: 'none' }}
+            this.props.article === "notre_activite" ? "active" : ""
+          } ${this.props.articleTimeout ? "timeout" : ""}`}
+          style={{ display: "none" }}
         >
-          <h2 className="major">Notre travail</h2>
+          <h2 className="major">Notre activité</h2>
           <span className="image main">
             <img src={pic1} alt="" />
           </span>
           <p>
-            Adipiscing magna sed dolor elit. Praesent eleifend dignissim arcu,
-            at eleifend sapien imperdiet ac. Aliquam erat volutpat. Praesent
-            urna nisi, fringila lorem et vehicula lacinia quam. Integer
-            sollicitudin mauris nec lorem luctus ultrices.
+            Située au Pian médoc, l’entreprise CREA SANIT vous accompagne dans
+            l’exécution de vos travaux:
+          </p>
+
+          <ul>
+            <li>Plomberie / Sanitaire</li>
+            <li>Salle de bains clé en main</li>
+            <li>Chauffage</li>
+            <li>Energies renouvelables</li>
+            <li>Dépannage</li>
+          </ul>
+
+          <p>
+            En neuf ou en rénovation, nous vous proposons des solutions pour
+            améliorer durablement votre confort et réaliser des économies
+            d’énergie.
           </p>
           <p>
-            Nullam et orci eu lorem consequat tincidunt vivamus et sagittis
-            libero. Mauris aliquet magna magna sed nunc rhoncus pharetra.
-            Pellentesque condimentum sem. In efficitur ligula tate urna.
-            Maecenas laoreet massa vel lacinia pellentesque lorem ipsum dolor.
-            Nullam et orci eu lorem consequat tincidunt. Vivamus et sagittis
-            libero. Mauris aliquet magna magna sed nunc rhoncus amet feugiat
-            tempus.
+            Entreprise artisanale expérimentée, elle a su accroître son
+            savoir-faire et se diversifier en se maintenant à la pointe de
+            l’innovation et des tendances. Une expérience qui fait sa réputation
+            et qui va de pair avec une recherche permanente de la qualité.
+          </p>
+          <p>
+            Personnalisation de vos projets, conseils fiables, relations de
+            confiance : telles sont nos valeurs pour vous aider à choisir parmi
+            toutes les solutions afin de valoriser votre habitat.
           </p>
           {close}
         </article>
-
         <article
-          id="about"
-          className={`${this.props.article === 'about' ? 'active' : ''} ${
-            this.props.articleTimeout ? 'timeout' : ''
-          }`}
-          style={{ display: 'none' }}
+          id="plomberie_sanitaire"
+          className={`${
+            this.props.article === "plomberie_sanitaire" ? "active" : ""
+          } ${this.props.articleTimeout ? "timeout" : ""}`}
+          style={{ display: "none" }}
         >
-          <h2 className="major">A propos</h2>
+          <h2 className="major">Plomberie/Sanitaire</h2>
           <span className="image main">
             <img src={pic2} alt="" />
           </span>
           <p>
-            Lorem ipsum dolor sit amet, consectetur et adipiscing elit. Praesent
-            eleifend dignissim arcu, at eleifend sapien imperdiet ac. Aliquam
-            erat volutpat. Praesent urna nisi, fringila lorem et vehicula
-            lacinia quam. Integer sollicitudin mauris nec lorem luctus ultrices.
-            Aliquam libero et malesuada fames ac ante ipsum primis in faucibus.
-            Cras viverra ligula sit amet ex mollis mattis lorem ipsum dolor sit
-            amet.
+            Forts de notre polyvalence et de notre expérience, nous vous
+            proposons des salles de bains « clé en main » à la fois
+            fonctionnelles et originales mais aussi des solutions pour les
+            personnes à mobilité réduite. Conception, agencement,
+            embellissement, rénovation: nos spécialistes offrent un bain de
+            jouvence à votre pièce d’eau. Nous accompagnons votre projet : que
+            vous souhaitiez remplacer votre baignoire par une douche à
+            l’italienne tendance et facile d'accès, créer une salle d’eau dans
+            une pièce inutilisée, optimiser un petit volume ou transformer une
+            grande pièce en espace bien-etre, nous relevons le défi !
+          </p>
+          <p>
+            Nous intervenons chez vous dans tous les corps de métiers :
+            alimentation et évacuation d’eau, pose de matériel sanitaire, pose
+            de faïence, étanchéité, électricité, etc. une seule entreprise, un
+            seul interlocuteur pour votre projet.
+          </p>
+          {close}
+        </article>
+        <article
+          id="chauffage_climatisation"
+          className={`${
+            this.props.article === "chauffage_climatisation" ? "active" : ""
+          } ${this.props.articleTimeout ? "timeout" : ""}`}
+          style={{ display: "none" }}
+        >
+          <h2 className="major">Chauffage/Climatisation</h2>
+          <span className="image main">
+            <img src={pic2} alt="" />
+          </span>
+          <p>
+            Nous travaillons avec des grandes marques professionnelles, gage de
+            qualité pour l’installation de divers appareils de chauffage comme
+            Atlantic, Daikin ou Saunier Duval pour les pompes à chaleur
+            (air/air, air/eau), les chaudières gaz, fioul et la climatisation
+            gainable ou split.
+          </p>
+          {close}
+        </article>{" "}
+        <article
+          id="depannage"
+          className={`${this.props.article === "depannage" ? "active" : ""} ${
+            this.props.articleTimeout ? "timeout" : ""
+          }`}
+          style={{ display: "none" }}
+        >
+          <h2 className="major">Dépannage</h2>
+          <span className="image main">
+            <img src={pic2} alt="" />
+          </span>
+          <p>
+            Une fuite d’eau?
+            <br />
+            L’entreprise prend en charge vos travaux de tuyauterie générale,
+            appareils de distribution d'eau et de gaz, installation de robinets,
+            de tuyaux d'évacuation pour les eaux usées, pose d'appareils de
+            traitement de l'eau ou d'adoucisseurs, service de maintenance,
+            nettoyage des canalisations, réparation, dépannage en cas de fuite.
+            Pour tous vos travaux de plomberie, faites confiance à une
+            entreprise d’expérience.
           </p>
           {close}
         </article>
       </div>
-    )
+    );
   }
 }
 
@@ -83,7 +149,7 @@ Main.propTypes = {
   articleTimeout: PropTypes.bool,
   onCloseArticle: PropTypes.func,
   timeout: PropTypes.bool,
-  setWrapperRef: PropTypes.func.isRequired,
-}
+  setWrapperRef: PropTypes.func.isRequired
+};
 
-export default Main
+export default Main;
